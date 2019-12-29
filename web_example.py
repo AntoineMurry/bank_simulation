798486d51d1d0a3f4606909174e1ae36374f94bc
Antoine <antoine.murry@mazars.fr>
2019-12-29 19:11:03 +0100

    add triangular law as teller time

--- web_example.py
+++ web_example.py
@@ -1,4 +1,4 @@
-from random import expovariate, seed
+from random import expovariate, triangular, seed
 
 import simpy
 from simpy.core import BaseEnvironment
@@ -7,7 +7,7 @@ from simpy.util import start_delayed
 
 ## Model components -----------------------------
 
-def visit(env, timeInBank, name, resource):
+def visit(env, tell_l_m_r, name, resource):
     """ Customer arrives, looks around and leaves """
     arrive = env.now
     print(arrive, name," Here I am")
@@ -15,15 +15,18 @@ def visit(env, timeInBank, name, resource):
     yield req
     wait = env.now - arrive  # waiting time
     print(env.now, name, 'waited :', wait)
-    yield env.timeout(timeInBank)
+    # tib = expovariate(1.0/timeInBank)
+    low, high, mode = tell_l_m_r
+    tib = triangular(low, high, mode)
+    yield env.timeout(tib)
     resource.release(req)
     print (env.now, name, " Finished")
 
 
-def generate(number, meanTBA, resource):
+def generate(number, meanTBA, resource, tell_l_m_r):
     """ Source generates customers regularly """
     for i in range(number):
-        env.process(visit(env, timeInBank=12,
+        env.process(visit(env, tell_l_m_r=tell_l_m_r,
                           name='customer_' + str(i),
                           resource=resource))
         t = expovariate(1.0/meanTBA)
@@ -34,7 +37,7 @@ def generate(number, meanTBA, resource):
 
 maxNumber = 5
 maxTime = 400.0     # minutes
-# timeInBank = 10.0   # minutes
+tell_l_m_r = (3, 30, 10)   # average time at counter, minutes
 meanTBA = 1.5   # average time between arrivals, minutes
 # meanTBA = 10
 
@@ -44,5 +47,5 @@ seed(0)
 env = simpy.Environment()
 counter = simpy.Resource(env, capacity=1)
 env.process(generate(number=maxNumber, meanTBA=meanTBA,
-                     resource=counter))
+                     resource=counter, tell_l_m_r=tell_l_m_r))
 env.run(until=maxTime)
