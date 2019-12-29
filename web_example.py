920270cef2fc670904894e5caa6a136bbb5e0402
Antoine <antoine.murry@mazars.fr>
2019-12-29 18:42:14 +0100

    add 1 resource: 1 teller

--- web_example.py
+++ web_example.py
@@ -1,61 +1,48 @@
-""" bank01: The single non-random Customer """
 from random import expovariate, seed
 
 import simpy
 from simpy.core import BaseEnvironment
-# from simpy.events import Initialize
-# from simpy.events import Process
 from simpy.util import start_delayed
 
 
 ## Model components -----------------------------
 
-def visit(env, timeInBank, name):
+def visit(env, timeInBank, name, resource):
     """ Customer arrives, looks around and leaves """
-    print(env.now, name," Here I am")
+    arrive = env.now
+    print(arrive, name," Here I am")
+    req = resource.request()
+    yield req
+    wait = env.now - arrive  # waiting time
+    print(env.now, name, 'waited :', wait)
     yield env.timeout(timeInBank)
-    print (env.now, name," I must leave")
+    resource.release(req)
+    print (env.now, name, " Finished")
 
 
-def generate(number, meanTBA):
+def generate(number, meanTBA, resource):
     """ Source generates customers regularly """
     for i in range(number):
-        # c = Customer(name = "Customer%02d"%(i,))
         env.process(visit(env, timeInBank=12,
-                          name='customer_' + str(i)))
-        t = expovariate(meanTBA)
+                          name='customer_' + str(i),
+                          resource=resource))
+        t = expovariate(1.0/meanTBA)
         yield env.timeout(t)
 
 
 ## Experiment data ------------------------------
 
 maxNumber = 5
-maxTime = 100.0     # minutes
+maxTime = 400.0     # minutes
 # timeInBank = 10.0   # minutes
-meanTBA = 0.66667   # average time between arrivals, minutes
+meanTBA = 1.5   # average time between arrivals, minutes
+# meanTBA = 10
 
 ## Model/Experiment ------------------------------
 
-RANDOM_SEED = 42
-# seed(RANDOM_SEED)
+seed(0)
 env = simpy.Environment()
-# c = Customer(name="Klaus")
-# activate(c,c.visit(timeInBank),at=5.0)
-# visit_1 = visit(env, timeInBank=timeInBank, name='Klaus')
-# visit_2 = visit(env, timeInBank=timeInBank, name='Charls')
-# visit_3 = visit(env, timeInBank=timeInBank, name='Bill')
-# proc = env.process(visit(env, timeInBank=timeInBank,
-#                          name='Klaus'))
-
-# activate(s, s.generate(number=maxNumber,
-#                        TBA=ARRint), at=0.0)
-env.process(generate(number=maxNumber, meanTBA=meanTBA))
-
-# proc1 = env.process(visit_1)
-# proc2 = env.process(visit_2)
-# proc3 = env.process(visit_3)
-
-# t = expovariate(0.66667)
-# proc = start_delayed(env, visit(env, timeInBank=timeInBank,
-#                                 name='Klaus'), t)
+counter = simpy.Resource(env, capacity=1)
+env.process(generate(number=maxNumber, meanTBA=meanTBA,
+                     resource=counter))
 env.run(until=maxTime)
