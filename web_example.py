3e64115253d6a7341bfa13b42be50d6686eca573
Antoine <antoine.murry@mazars.fr>
2019-12-30 11:59:41 +0100

    clean code and add docstrings & comments

--- web_example.py
+++ web_example.py
@@ -5,10 +5,8 @@ from simpy.core import BaseEnvironment
 from simpy.util import start_delayed
 
 
-## Model components -----------------------------
+## Model functions
 
-WAIT_TIMES_ctrs = []
-WAIT_TIMES_ATM = []
 
 def otr_clts_in_res(res):
     """ number of clients waiting for or with resource res"""
@@ -16,98 +14,132 @@ def otr_clts_in_res(res):
 
 
 def visit_cntr(env, tell_l_m_r, name, counters):
-    """ Customer arrives, stands in line, goes to teller and leaves """
+    """
+    Customer arrives, stands in line, goes to teller and leaves
+    :input:
+        env: environment of simulation (simpy object)
+        tell_l_m_r: tuple that contains triangular law parameters
+        name: name of customer
+        counters: list of teller resources in the simulation
+    """
+    # time when customer arrives
     arrive = env.now
     num_counters = len(counters)
+    # list the queues of all other tellers (counters)
+    # then choose the teller with the shortest queue
     queues_len = [otr_clts_in_res(counters[i]) for i in range(num_counters)]
-    print('Here I am: ', env.now, name, queues_len)
     for i in range(num_counters):
         if (queues_len[i] == 0) or (queues_len[i] == min(queues_len)):
-            choice = i  # the chosen queue number
+            choice = i
             break
+    # request a teller resource
     req = counters[choice].request()
-    yield req
+    yield req # wait till resource can be allocated
 
+    # count time of use of teller
+    start_use_ctr = env.now
+
+    # draw teller tile from triangular law
     low, high, mode = tell_l_m_r
     tib = triangular(low, high, mode)
-    yield env.timeout(tib)
-    wait = env.now - arrive  # waiting time
+    yield env.timeout(tib) # wait till teller time is over
+
+    # count time client spend in line plus with teller
+    wait = env.now - arrive
     WAIT_TIMES_ctrs.append(wait)
-    print(env.now, name, ' finished total time at counter')
+    # release teller resource
     counters[choice].release(req)
-    print (env.now, name, " Finished")
+    CTR_TIME.append(env.now - start_use_ctr)
 
 
 def visit_atm(env, atm_l_m_r, name, atms):
-    """ Customer arrives, stands in line, goes to atm and leaves """
+    """
+    Customer arrives, stands in line, goes to atm and leaves
+    :input:
+        env: environment of simulation (simpy object)
+        atm_l_m_r: tuple that contains triangular law parameters
+        name: name of customer
+        atms: list of atm resources in the simulation
+    """
+    # time when customer arrives
     arrive_atm = env.now
     num_atms = len(atms)
+    # list the queues of all other atms
+    # then choose the atm with the shortest queue
     queues_len = [otr_clts_in_res(atms[i]) for i in range(num_atms)]
-    print('Here I am: ', env.now, name, queues_len)
     for i in range(num_atms):
         if (queues_len[i] == 0) or (queues_len[i] == min(queues_len)):
-            choice = i  # the chosen queue number
+            choice = i
             break
+    # request an atm resource
     req = atms[choice].request()
     yield req
-    wait = env.now - arrive_atm  # waiting time
-    WAIT_TIMES_ATM.append(wait)
-    print(env.now, name, 'finised atm line :', wait)
-    print(env.now, name, " Finished atm line")
 
-    low, high, mode = tell_l_m_r
+    # count time of use of atm
+    start_use_atm = env.now
+    # save time spend in atm line
+    wait = env.now - arrive_atm
+    WAIT_TIMES_ATM.append(wait)
+
+    # draw teller tile from triangular law
+    low, high, mode = atm_l_m_r
     tib = triangular(low, high, mode)
     yield env.timeout(tib)
     atms[choice].release(req)
-    print (env.now, name, " Finished")
+    # release ATM resource
+    ATM_TIME.append(env.now - start_use_atm)
 
 
 def visit_atm_ctr(env, tell_l_m_r, name, counters, atm_l_m_r, atms):
     """
     Customer arrives, stands in line, goes to atm, then teller and leaves
+    :input:
+        env: environment of simulation (simpy object)
+        atm_l_m_r: tuple that contains triangular law parameters for atm
+        tell_l_m_r: tuple that contains triangular law parameters for teller
+        name: name of customer
+        atms: list of atm resources in the simulation
+        counters: list of counter resources in the simulation
     """
+    # atm part (same principle as visit_atm function)
     arrive_atm = env.now
     num_atms = len(atms)
     queues_len = [otr_clts_in_res(atms[i]) for i in range(num_atms)]
-    print('Here I am: ', env.now, name, queues_len)
     for i in range(num_atms):
         if (queues_len[i] == 0) or (queues_len[i] == min(queues_len)):
-            choice = i  # the chosen queue number
+            choice = i
             break
     req = atms[choice].request()
     yield req
-    wait = env.now - arrive_atm  # waiting time
+    start_use_atm = env.now
+    wait = env.now - arrive_atm
     WAIT_TIMES_ATM.append(wait)
-    print(env.now, name, 'finised atm line :', wait)
-    print(env.now, name, 'Finished ATM line')
 
-    low, high, mode = tell_l_m_r
+    low, high, mode = atm_l_m_r
     tib = triangular(low, high, mode)
     yield env.timeout(tib)
     atms[choice].release(req)
+    ATM_TIME.append(env.now - start_use_atm)
 
-
+    # teller part (same principle as visit_cntr function)
     arrive_ctr = env.now
     num_counters = len(counters)
     queues_len = [otr_clts_in_res(counters[i]) for i in range(num_counters)]
-    print('After atm, I am now going to teller: ', env.now, name, queues_len)
     for i in range(num_counters):
         if (queues_len[i] == 0) or (queues_len[i] == min(queues_len)):
-            choice = i  # the chosen queue number
+            choice = i
             break
     req = counters[choice].request()
     yield req
+    start_use_ctr = env.now
 
     low, high, mode = tell_l_m_r
     tib = triangular(low, high, mode)
     yield env.timeout(tib)
-    wait = env.now - arrive_ctr  # waiting time
+    wait = env.now - arrive_ctr
     WAIT_TIMES_ctrs.append(wait)
-    print(env.now, name, 'total teller time :', wait)
     counters[choice].release(req)
-    print (env.now, name, " Finished")
-
-
+    CTR_TIME.append(env.now - start_use_ctr)
 
 
 def generate(number, meanTBA, counters, tell_l_m_r, atms, atm_l_m_r):
@@ -131,21 +163,37 @@ def generate(number, meanTBA, counters, tell_l_m_r, atms, atm_l_m_r):
         yield env.timeout(t)
 
 
-## Experiment data ------------------------------
+## Model/Experiment
 
 maxNumber = 50000
 maxTime = 72000.0     # minutes
 tell_l_m_r = (3, 30, 10)   # average time at counter, minutes
 atm_l_m_r = (2, 10, 20) # average time at atm, minutes
 meanTBA = 1.5   # average time between arrivals, minutes
-# meanTBA = 10
-
-## Model/Experiment ------------------------------
 
 # seed(0)
-env = simpy.Environment()
-counters = [simpy.Resource(env, capacity=1),
-            simpy.Resource(env, capacity=1),
+
+avgs_wait_atm = []
+avgs_wait_ctrs = []
+
+for i in range(5):
+    WAIT_TIMES_ctrs = []
+    WAIT_TIMES_ATM = []
+    CTR_TIME = []
+    ATM_TIME = []
+    env = simpy.Environment()
+    counters = [simpy.Resource(env, capacity=1),
+                simpy.Resource(env, capacity=1),
+                simpy.Resource(env, capacity=1),
+                simpy.Resource(env, capacity=1),
+                simpy.Resource(env, capacity=1),
+                simpy.Resource(env, capacity=1),
+                simpy.Resource(env, capacity=1),
+                simpy.Resource(env, capacity=1),
+                simpy.Resource(env, capacity=1),
+                simpy.Resource(env, capacity=1),
+                simpy.Resource(env, capacity=1)]
+    atms = [simpy.Resource(env, capacity=1),
             simpy.Resource(env, capacity=1),
             simpy.Resource(env, capacity=1),
             simpy.Resource(env, capacity=1),
@@ -156,21 +204,18 @@ counters = [simpy.Resource(env, capacity=1),
             simpy.Resource(env, capacity=1),
             simpy.Resource(env, capacity=1),
             simpy.Resource(env, capacity=1)]
-atms = [simpy.Resource(env, capacity=1),
-        simpy.Resource(env, capacity=1),
-        simpy.Resource(env, capacity=1),
-        simpy.Resource(env, capacity=1),
-        simpy.Resource(env, capacity=1),
-        simpy.Resource(env, capacity=1),
-        simpy.Resource(env, capacity=1),
-        simpy.Resource(env, capacity=1)]
-env.process(generate(number=maxNumber, meanTBA=meanTBA,
-                     counters=counters, tell_l_m_r=tell_l_m_r,
-                     atms=atms, atm_l_m_r=atm_l_m_r))
-env.run(until=maxTime)
 
-print('average time for ATM line: ',
-      sum(WAIT_TIMES_ATM) / len(WAIT_TIMES_ATM))
+    env.process(generate(number=maxNumber, meanTBA=meanTBA,
+                         counters=counters, tell_l_m_r=tell_l_m_r,
+                         atms=atms, atm_l_m_r=atm_l_m_r))
+    env.run(until=maxTime)
+    avg_atm = sum(WAIT_TIMES_ATM) / len(WAIT_TIMES_ATM)
+    avg_ctrs = sum(WAIT_TIMES_ctrs) / len(WAIT_TIMES_ctrs)
+    print('average time for ATM line: ', avg_atm)
+    print('average time for counters: ', avg_ctrs)
+    avgs_wait_atm.append(avg_atm)
+    avgs_wait_ctrs.append(avg_ctrs)
+    print(env.now, ': time')
 
-print('average time for counters: ',
-      sum(WAIT_TIMES_ctrs) / len(WAIT_TIMES_ctrs))
+print(sum(avgs_wait_atm)/len(avgs_wait_atm), ': average wait atm')
+print(sum(avgs_wait_ctrs)/len(avgs_wait_ctrs), ': average wait counters')
